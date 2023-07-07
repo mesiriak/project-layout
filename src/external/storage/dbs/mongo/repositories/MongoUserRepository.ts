659451dff38f23da3e58e47d16a4822558670a33
Max Mesiriak <mesiriak.work@gmail.com>
2023-07-07 12:31:31 +0300

    onion: init (lost files)

--- src/external/storage/dbs/mongo/repositories/MongoUserRepository.ts
+++ src/external/storage/dbs/mongo/repositories/MongoUserRepository.ts
@@ -1,5 +1,27 @@
-import { UserRepository } from "../../../../core/domainServices/UserRepository"
+import { User } from "../../../../../core/domain/User"
+import {
+  CreateUserDTO,
+  UpdateUserDTO,
+} from "../../../../../core/domainServices/DTO/UserDTO"
+import { UserRepository } from "../../../../../core/domainServices/UserRepository"
 
 export class MongoUserRepository implements UserRepository {
-  constructor(parameters) {}
+  getList(limit: number, offset: number): [number, User[]] {
+    throw new Error("Method not implemented.")
+  }
+  getById(id: number): User {
+    throw new Error("Method not implemented.")
+  }
+  getByTitle(title: string): User {
+    throw new Error("Method not implemented.")
+  }
+  createOne(dto: CreateUserDTO): User {
+    throw new Error("Method not implemented.")
+  }
+  updateOne(dto: UpdateUserDTO): User {
+    throw new Error("Method not implemented.")
+  }
+  deleteOne(id: number): void {
+    throw new Error("Method not implemented.")
+  }
 }
